2cccb536f77c33caf817db4b1106b387a13241c6
Orion Weldon <orion@Orions-MacBook-Pro.local>
2018-02-20 15:07:37 -0800

    registers mic volume and resizes ellipses accordingly

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var mic;
-function setup(){
-  createCanvas(200, 200);
-  mic = new p5.AudioIn();
-  mic.start();
-}
+
+ function setup() {
+   createCanvas(500, 500);
+   mic = new p5.AudioIn();
+   amp = new p5.Amplitude();
+   mic.start();
+ }
+
+ function draw() {
+   background(0);
+   var vol = mic.getLevel();
+   var diam = map(vol, 0, 0.3, 10, 200);
+   fill(255, 0, 255);
+   ellipse(width / 2, height / 2, diam, diam);
+ }
