26fda656ca1a48432476534290cf0a281bf75a47
Orion Weldon <weldon.orion@gmail.com>
2018-02-22 10:51:37 -0800

    getEnergy is working. Shapes change size based on frequency

--- index.js
+++ index.js
@@ -1,76 +1,80 @@
 var mic;
-var fft;
 var analyzer;
 var fft;
 var peakDetect;
 var peaks;
 
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(700, 500);
     colorMode(HSB);
     mic = new p5.AudioIn();
-    fft = new p5.FFT(0.9, 512);
+    fft = new p5.FFT(0.9, 1024);
     analyzer = new p5.Amplitude();
     peakDetect = new p5.PeakDetect();
     mic.start();
     fft.setInput(mic);
     analyzer.setInput(mic);
-    // peaks = mic.getPeaks();
 }
-
-var r = 0, g = 0, b = 0, o = 0;
 
 function draw() {
     background(0);
     var spectrum = fft.analyze();
-    peakDetect.update(fft);
-    fill('rgba(50,50,50,1)');
-    stroke('rgba(50,50,50,1)');
-    strokeWeight(1);
+    // peakDetect.update(fft);
 
-    for (var i = 0; i< spectrum.length; i+=10){
+    //bargraph
+    for(var i = 0; i< spectrum.length; i+=10){
         var x = map(i, 0, spectrum.length, 0, width);
         var h = -height + map(spectrum[i], 0, 255, height, 0);
-        fill(i, 255, 255)
-        rect(x, height, (width / spectrum.length) * 10, h )
+        fill(i, 255, 255);
+        rect(x, height, (width / spectrum.length) * 10, h );
     }
 
+    //waveform
     var waveform = fft.waveform();
     noFill();
     beginShape();
 
-    stroke(255,255,255); // waveform is white
-    strokeWeight(1);
-    for (var i = 0; i< waveform.length; i+=10){
+    stroke(255,255,255);
+    strokeWeight(3);
+    for(var i = 0; i< waveform.length; i+=10){
         var x = map(i, 0, waveform.length, 0, width);
         var y = map( waveform[i]/10, -1, 1, 0, height);
         vertex(x,y);
     }
     endShape();
 
-    var rms = 0;
-    if ( peakDetect.isDetected ) {
-        rms = analyzer.getLevel();
-        r = 127;
-        g = -127;
-        b = -188;
-        o = .5;
-
-    } else {
-        r = r * .85;
-        g = g * .85;
-        b = b * .85;
-        o = o * .85;
-        rms = analyzer.getLevel();
-    }
-    fill('rgba('+parseInt(127 + r)+','+parseInt(127 + g)+','+parseInt(127 + b)+','+(.5 + o)+')');
-    fill('rgb(159, 6, 170)');
-    // Get the average (root mean square) amplitude
+    //ellipse
     var rms = analyzer.getLevel();
+    if(rms > 0.01){
+      fill('purple')
+    }else {
+      fill('blue');
+    }
+    stroke(1);
+    strokeWeight(1);
+    ellipse(width / 2, height / 2, 20 + rms * (height / 2), 20 + rms * (height / 2));
 
-    stroke(0);
-    strokeWeight(0);
+    var bass = fft.getEnergy('lowMid');
+    console.log(bass)
+    if(bass < 400){
+      stroke(1);
+      fill('white');
+      beginShape();
+      ellipse(width / 2, height / 2, bass/150 * (height / 2), bass/150 * (height / 2));
 
-    // Draw an ellipse with size based on volume
-    ellipse(width / 2, height / 2, 10 + rms * (height / 2), 10 + rms * (height / 2));
+      endShape();
+    }
+    // console.log("bass:" + bass);
+    var highmid = fft.getEnergy('highMid');
+    if(highmid < 200){
+      stroke(1);
+      fill('red');
+      beginShape();
+      ellipse(50, 50, highmid/200 * (height / 2), highmid/200 * (height / 2));
+
+      endShape();
+    }
+    // console.log("highmid:" + highmid);
+    var treble = fft.getEnergy('treble');
+    // console.log("treble:" + treble);
 }
