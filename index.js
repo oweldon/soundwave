8333c380d5e69509e6bcefb11ddf684033975829
Orion Weldon <weldon.orion@gmail.com>
2018-02-27 16:16:26 -0800

    removed particles

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var volhistory = [];
 
 function setup() {
     createCanvas(700, 500);
-    system = new ParticleSystem(createVector(width/2, 50));
     colorMode(RGB);
     angleMode(DEGREES);
     mic = new p5.AudioIn();
@@ -22,22 +21,7 @@ function setup() {
 
 function draw() {
     background(0);
-    system.addParticle();
-    system.run();
     var spectrum = fft.analyze();
-
-
-  // var x = width * noise(t);
-  // var y = height * noise(t+5);
-  // var r = 255 * noise(t+10);
-  // var g = 255 * noise(t+15);
-  // var b = 255 * noise(t+20);
-  //
-  // noStroke();
-  // fill(r, g, b);
-  // ellipse(x, y, 120, 120);
-  //
-  // t = t + 0.01;
 
     // bargraph
     for(var i = 0; i< spectrum.length; i+=10){
@@ -120,59 +104,3 @@ function draw() {
       endShape();
     }
 }
-
-// Particles
-var Particle = function(position) {
-  this.acceleration = createVector(0, 0.05);
-  this.velocity = createVector(random(-1, 1), random(-1, 0));
-  this.position = position.copy();
-  this.lifespan = 255.0;
-};
-
-Particle.prototype.run = function() {
-  this.update();
-  this.display();
-};
-
-// Method to update position
-Particle.prototype.update = function(){
-  this.velocity.add(this.acceleration);
-  this.position.add(this.velocity);
-  this.lifespan -= 2;
-};
-
-// Method to display
-Particle.prototype.display = function() {
-  stroke(200, this.lifespan);
-  strokeWeight(2);
-  fill(0, 255, 0, this.lifespan);
-  ellipse(this.position.x, this.position.y, 12, 12);
-};
-
-// Is the particle still useful?
-Particle.prototype.isDead = function(){
-  if (this.lifespan < 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-var ParticleSystem = function(position) {
-  this.origin = position.copy();
-  this.particles = [];
-};
-
-ParticleSystem.prototype.addParticle = function() {
-  this.particles.push(new Particle(this.origin));
-};
-
-ParticleSystem.prototype.run = function() {
-  for (var i = this.particles.length-1; i >= 0; i--) {
-    var p = this.particles[i];
-    p.run();
-    if (p.isDead()) {
-      this.particles.splice(i, 1);
-    }
-  }
-};
